6deec62b668299dde9e00d4a0befbc0a8270be0c
Yun-Chi Hsiao <is0.jimhsiao@gmail.com>
2021-10-31 02:18:30 +0800

    Update: Test passed

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(n){
+  if (n === 199) {
+    return 'This one is on me!';
+  } else if (n === 2001) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (n === 2501) {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(n){
+  if (n === "NYC") {
+    return ('Ok, sounds good.');
+  } else if (n === ('Pittsburgh')) {
+    return 'No go.';
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(t){
+  if (t === 'generous') {
+    return 'Thank you so much.';
+  } else if (t === 'not as generous') {
+    return 'Thank you.';
+  } else if (t === 'thanks for everything') {
+    return "Bye.";
+  }
 }
